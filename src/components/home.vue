ae519f31d7c8e742e6810078508e62b9734b3bd0
lework <lework@yeah.net>
2019-09-20 14:20:47 +0800

    feat: add search

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,14 +2,14 @@
   <div class="home">
     <div class="header">
       <div class="container">
-        <span class="title"><a href="#">LE-Version</a></span>
+        <span class="title"><a href="./">LE-Version</a></span>
         <span class="title-desc"> 列出开源软件的当前版本</span>
       </div>
     </div>
     <div class="main">
       <a-back-top />
       <div class="search">
-        <a-input-search placeholder="输入user/repo,Container"
+        <a-input-search placeholder="输入user/repo or type"
                         v-model="search_text"
                         @search="onSearch"
                         enterButton="搜索..." />
@@ -30,7 +30,8 @@
         <a-spin :spinning="spinning">
           <a-list itemLayout="horizontal"
                   :pagination="pagination"
-                  :dataSource="listData">
+                  :dataSource="listData"
+                  v-if="listData.length !== 0">
             <div slot="header"><b>更新时间: </b>{{ updated_at.toLocaleString() }} <b> 统计数目: </b>{{ total }}
               <div class="header-switch">
                 <a-switch checkedChildren="Shields"
@@ -46,7 +47,7 @@
                   托管: <a-tag color="#8CD790">{{ item.hosting }}</a-tag>
                   类别: <a-tag color="#2db7f5"
                          @click="tagChange(item.type)">{{ item.type }}</a-tag>
-                  当前版本: <a-tooltip placement="top"
+                  当前{{ item.html_url.indexOf('release') !== -1 ? '版本' : 'Tag' }}: <a-tooltip placement="top"
                              :title="item.created_at">
                     <a-tag color="#108ee9"
                            @click="tagClick(item.project, item.body)">{{ item.name || item.tag_name }}</a-tag>
@@ -54,7 +55,7 @@
                   <a-tooltip placement="top"
                              title="Last 10">
                     <a-tag color="#5bd1d7"
-                           @click="lastClick(item.project, item.html_url, item.repo)">最近版本</a-tag>
+                           @click="lastClick(item.project, item.html_url, item.repo)">最近{{ item.html_url.indexOf('release') !== -1 ? '版本' : 'Tag' }}</a-tag>
                   </a-tooltip>
                   创建时间: <a-tag color="#F17F42">{{ item.created_at || "None"  }}</a-tag>
                   <span v-if="shieldsShow">shield: </span>
@@ -70,7 +71,7 @@
                     <a-spin :spinning="versionSpinning">
                       <div v-if="showLast">
                         <a-divider orientation="left">
-                          <span style="font-size: 16px;">最近10个版本</span>
+                          <span style="font-size: 16px;">最近10个{{ item.html_url.indexOf('release') !== -1 ? '版本' : 'Tag' }}</span>
                         </a-divider>
                         <a-timeline>
                           <template v-for="(last, index) in lastData">
@@ -98,6 +99,41 @@
                    class="list-title"
                    target="_blank"
                    :href="item.repo_url">{{ item.project[0].toUpperCase() + item.project.slice(1) }}</a>
+              </a-list-item-meta>
+            </a-list-item>
+          </a-list>
+          <a-list itemLayout="horizontal"
+                  :dataSource="searchData"
+                  v-if="searchData.length !== 0 && listData.length === 0">
+            <div slot="header">在下列中选择你的项目吧! 本次搜索到了<b>{{ searchCount }}</b>个，只显示star最高的10个.
+            </div>
+            <a-list-item slot="renderItem"
+                         slot-scope="item, index">
+              <a-list-item-meta>
+                <div slot="description">
+                  {{item.description}}
+                  <p />
+                  <p>
+                    <a-icon type="star"
+                            theme="filled" /> {{item.stargazers_count}}
+                    <a-icon type="fork" /> {{item.forks_count}}
+                    <a-icon type="code"
+                            theme="filled" /> {{item.language}}
+                    <a-tooltip placement="top">
+                      <template slot="title">
+                        <span>最近更新</span>
+                      </template>
+                      <a-icon type="calendar"
+                              theme="filled" /> {{item.updated_at}}
+                    </a-tooltip>
+                    <span @click="tagChange(item.full_name)">查看版本</span>
+                  </p>
+                </div>
+
+                <a slot="title"
+                   class="list-title"
+                   target="_blank"
+                   :href="item.html_url">{{item.full_name}}</a>
               </a-list-item-meta>
             </a-list-item>
           </a-list>
@@ -137,7 +173,9 @@ export default {
       showInfo: false,
       showLast: false,
       versionSpinning: true,
-      lastData: []
+      lastData: [],
+      searchData: [],
+      searchCount: 0
     }
   },
   watch: {
@@ -151,36 +189,9 @@ export default {
         return
       }
       if (this.project !== project && url.indexOf('release') !== -1) {
-        let repoUrl = 'https://api.github.com/repos/' + repo + '/releases?per_page=10'
-        this.$axios.get(repoUrl).then((res) => {
-          this.lastData = res.data
-          this.versionSpinning = false
-        }).catch((e) => {
-          this.$message.error('获取数据失败')
-          console.log(e)
-        })
+        this._getGithubRelases(repo)
       } else if (this.project !== project && url.indexOf('commit') !== -1) {
-        let graphqlUrl = 'https://api.github.com/graphql'
-        let postData = {
-          'query': '{repository(owner: "' + repo.split('/')[0] + '", name: "' + repo.split('/')[1] + '") {refs(refPrefix: "refs/tags/", first: 10, orderBy: {field: TAG_COMMIT_DATE, direction: DESC}) {edges {node {name target {commitUrl ... on Tag {tagger {date}} ... on Commit {committedDate}}}}}}}'
-        }
-        let token = '99510f2ccf40e496d1e97dbec9f31cb16770b884'
-        let headers = { 'Content-Type': 'application/json; charset=utf-8', 'Authorization': 'token ' + token }
-        this.$axios.post(graphqlUrl, postData, { headers }).then((res) => {
-          let lastTags = res.data['data']['repository']['refs']['edges']
-          this.lastData = []
-          for (let item in lastTags) {
-            this.lastData.push({
-              'tag_name': lastTags[item]['node']['name'],
-              'html_url': lastTags[item]['node']['target']['commitUrl'] || '',
-              'created_at': lastTags[item]['node']['target'].hasOwnProperty('tagger') ? lastTags[item]['node']['target']['tagger']['date'] : lastTags[item]['node']['target']['committedDate']
-            })
-          }
-          this.versionSpinning = false
-        }).catch((e) => {
-          this.$message.error('获取数据失败')
-          console.log(e)
-        })
+        this._getGithubTags(repo, 'lastData')
       } else {
         this.versionSpinning = false
       }
@@ -222,6 +233,7 @@ export default {
       }
     },
     _getData (search = '') {
+      this.spinning = true
       this.$axios.get('static/data/data.json').then((rep) => {
         this.listData = rep.data['data']
         this.total = rep.data['total']
@@ -233,16 +245,74 @@ export default {
             }
           }
           if (this.listData.length === 0) {
-            this._getGithub(search)
+            this._getGithubLatest(search)
+            if (this.listData.length === 0) {
+              this._getGithubTags(search, 'listData')
+            }
+            if (this.listData.length === 0) {
+              this._getSearchGithub(search)
+            }
+          } else {
+            this.spinning = false
           }
+        } else {
+          this.spinning = false
         }
-        this.spinning = false
       }).catch((e) => {
         this.$message.error('获取数据失败')
         console.log(e)
       })
     },
-    _getGithub (repo) {
+    _getGithubTags (repo, target) {
+      let graphqlUrl = 'https://api.github.com/graphql'
+      let postData = {
+        'query': '{repository(owner: "' + repo.split('/')[0] + '", name: "' + repo.split('/')[1] + '") {refs(refPrefix: "refs/tags/", first: 10, orderBy: {field: TAG_COMMIT_DATE, direction: DESC}) {edges {node {name target {commitUrl ... on Tag {message tagger {date}} ... on Commit {committedDate}}}}}}}'
+      }
+      let token = '99510f2ccf40e496d1e97dbec9f31cb16770b884'
+      let headers = { 'Content-Type': 'application/json; charset=utf-8', 'Authorization': 'token ' + token }
+      this.$axios.post(graphqlUrl, postData, { headers }).then((res) => {
+        if (res.data['data']['repository'] !== null && res.data['data']['repository'].hasOwnProperty('refs')) {
+          let lastTags = res.data['data']['repository']['refs']['edges']
+          this[target] = []
+          for (let item in lastTags) {
+            this[target].push({
+              'tag_name': lastTags[item]['node']['name'],
+              'html_url': lastTags[item]['node']['target']['commitUrl'] || '',
+              'created_at': lastTags[item]['node']['target'].hasOwnProperty('tagger') ? lastTags[item]['node']['target']['tagger']['date'] : lastTags[item]['node']['target']['committedDate'],
+              'project': repo,
+              'repo': repo,
+              'body': lastTags[item]['node']['target']['message'],
+              'hosting': 'github',
+              'type': 'None'
+            })
+            if (target === 'listData') {
+              this.total = this.listData.length
+              this.updated_at = new Date()
+              break
+            }
+          }
+        }
+        if (this[target].length === 0) {
+          this.$message.error('未找到项目的tags')
+        }
+        this.versionSpinning = false
+        this.spinning = false
+      }).catch((e) => {
+        this.$message.error('获取项目的tags失败')
+        console.log(e)
+      })
+    },
+    _getGithubRelases (repo) {
+      let repoUrl = 'https://api.github.com/repos/' + repo + '/releases?per_page=10'
+      this.$axios.get(repoUrl).then((res) => {
+        this.lastData = res.data
+        this.versionSpinning = false
+      }).catch((e) => {
+        this.$message.error('获取项目的releases失败')
+        console.log(e)
+      })
+    },
+    _getGithubLatest (repo) {
       let repoUrl = 'https://api.github.com/repos/' + repo + '/releases/latest'
       this.$axios.get(repoUrl).then((res) => {
         let data = {
@@ -253,14 +323,39 @@ export default {
           'body': res.data['body'],
           'created_at': res.data['created_at'],
           'project': repo,
+          'repo': repo,
           'hosting': 'github',
           'type': 'None'
         }
         this.listData = [data]
         this.total = this.listData.length
         this.updated_at = new Date()
+        this.spinning = false
       }).catch((e) => {
-        this.$message.error('获取数据失败')
+        this.$message.error('没有找到项目的latest release版本！')
+        console.log(e)
+      })
+    },
+    _getSearchGithub (repo) {
+      let searchUrl = 'https://api.github.com/search/repositories?q=' + repo + '+in:name&sort=stars&order=desc&per_page=10'
+      this.$axios.get(searchUrl).then((res) => {
+        this.searchData = []
+        this.searchCount = res.data['total_count']
+        for (let item in res.data['items']) {
+          let data = {
+            'full_name': res.data['items'][item]['full_name'],
+            'html_url': res.data['items'][item]['html_url'],
+            'description': res.data['items'][item]['description'],
+            'forks_count': res.data['items'][item]['forks_count'],
+            'stargazers_count': res.data['items'][item]['stargazers_count'],
+            'language': res.data['items'][item]['language'],
+            'updated_at': res.data['items'][item]['updated_at']
+          }
+          this.searchData.push(data)
+        }
+        this.spinning = false
+      }).catch((e) => {
+        this.$message.error('没有搜索到项目！')
         console.log(e)
       })
     }
